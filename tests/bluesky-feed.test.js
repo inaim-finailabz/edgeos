c277a7b718107338d28d4479b5f415377a96bffe
Issam <issam@docaidbot.com>
2026-07-16 00:24:54 +0100

    Keep Bluesky feed tests outside Netlify functions

--- tests/bluesky-feed.test.js
+++ tests/bluesky-feed.test.js
@@ -1,6 +1,6 @@
 const test = require("node:test");
 const assert = require("node:assert/strict");
-const { parseRanking, deterministicScore } = require("./_bluesky-feed");
+const { parseRanking, deterministicScore } = require("../site/netlify/functions/_bluesky-feed");
 
 test("parseRanking extracts valid unique indices", () => {
   assert.deepEqual(parseRanking('result: [{"index":2,"score":99},{"index":0,"score":80},{"index":2,"score":1}]', 3), [2, 0]);
